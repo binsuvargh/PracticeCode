c38f778097c7b60796b1ce083f8ecc1c4aa5177b
Binsu Varghese <binsu.varghese@metlife.com>
2021-07-27 17:27:09 +0530

    logging changes

---
+++ ConsoleApp2/LogHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using log4net;
+
+namespace ConsoleApp2
+{
+    public class LogHelper
+    {
+        public static ILog GetLogger([CallerFilePath] string filename = "")
+        {
+            return LogManager.GetLogger(filename);
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
@@ -10,14 +11,21 @@ namespace ConsoleApp2
 {
     class Program
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+       // private static readonly ILog log = LogHelper.GetLogger();
+        private static readonly ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        //private static readonly ILog log = LogManager.GetLogger(string.Empty);
         static void Main(string[] args)
         {
             //log.Debug("Developer: Tutorial was run");
             //log.Info("Maintenance: water pump turned on");
             //log.Warn("Maintenance: the water pump is getting hot");
+            var code = "code";
+            var policyId = 123456;
+            var customerId = 90876;
+            var metRefId = 1;
 
-            
+            log.Debug($"Generated url code: {code} for policy with id: {policyId},customer id: {customerId}, MetRefId: {metRefId}");
 
             Console.WriteLine("hello");
             var i = 0;
@@ -28,7 +36,7 @@ namespace ConsoleApp2
             }
             catch (DivideByZeroException ex)
             {
-                log.Error("developer: we tried to divide by zero again", ex);
+                log.Error($"developer: we tried to divide by zero again {ex.Message}", ex);
             }
             Console.ReadLine();
         }
